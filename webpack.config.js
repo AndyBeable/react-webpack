e7c24873ae2d0710faf64c181cd46c3e5e72cad8
AndyBeable <andy.beable@gmail.com>
2021-02-16 12:25:44 +0000

    file ext change

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/index.ts',
+  entry: './app/index.tsx',
   output: {
     filename: 'myBundle.js',
     path: path.resolve(__dirname, 'dist'),
